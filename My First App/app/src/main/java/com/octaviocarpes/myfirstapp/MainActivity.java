5a37897131faa9d843956adb11f38fd62b42aaa0
octavio.carpes <octavio.carpes10@gmail.com>
2018-07-29 10:00:44 -0300

    Finished first app

--- My First App/app/src/main/java/com/octaviocarpes/myfirstapp/MainActivity.java
+++ My First App/app/src/main/java/com/octaviocarpes/myfirstapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.octaviocarpes.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TOTAL_COUNT = "total_count";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +23,12 @@ public class MainActivity extends AppCompatActivity {
      */
     public void toastMe(View view){
         // Toast myToast = Toast.makeText(this, message, duration);
-        Toast myToast = Toast.makeText(this, "Hello Toast!",
-                Toast.LENGTH_SHORT);
+        Toast myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT);
         myToast.show();
     }
 
     public void countMe(View view){
-        TextView textView = (TextView) findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.textView);
 
         String countString = textView.getText().toString();
 
@@ -37,5 +39,14 @@ public class MainActivity extends AppCompatActivity {
         countString = count.toString();
 
         textView.setText(countString);
+    }
+
+    public void randomMe(){
+        TextView textView = findViewById(R.id.textView);
+        Integer count = Integer.parseInt(textView.getText().toString());
+
+        Intent randomIntent = new Intent(this, SecondActivity.class);
+        randomIntent.putExtra(TOTAL_COUNT, count);
+        startActivity(randomIntent);
     }
 }
